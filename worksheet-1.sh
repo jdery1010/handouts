65f47c27ad98871a92690cb314c4f54b8cb090cb
jdery1010 <jdery@email.arizona.edu>
2017-08-01 16:30:42 +0000

    test commit

--- worksheet-1.sh
+++ worksheet-1.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## Configure git
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name jdery1010
+git config --global user.email jdery@email.arizona.edu
 
 ## Change the "origin" remote URL
-git remote set-url origin ...
+#git remote set-url origin...
 
 ## Set the SESYNC-CI repository upstream and pull updates
-git remote add upstream https://github.com/sesync-ci/handouts.git
-git pull upstream master
+#git remote add upstream https://github.com/sesync-ci/handouts.git
+#git pull upstream master
